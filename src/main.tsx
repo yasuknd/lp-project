55db06211b3f4e0d2ad063a33032e46f698adfc3
yasuknd <yasu.knd@gmail.com>
2026-05-18 11:50:09 +0900

    feat: /design ルートを追加しAppDesign.tsxを表示

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/index.css";
 import App from "./app/App";
+import AppDesign from "./app/AppDesign";
 import PasswordGate from "./components/PasswordGate";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <PasswordGate>
-      <App />
+      <BrowserRouter basename="/lp-project">
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/design" element={<AppDesign />} />
+        </Routes>
+      </BrowserRouter>
     </PasswordGate>
   </StrictMode>
 );
