5a45ef69f424c52a84936519e9585897216d7960
yasuknd <yasu.knd@gmail.com>
2026-05-18 09:28:05 +0900

    viteベースパス設定

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/lp-project/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
